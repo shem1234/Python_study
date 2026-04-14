955ff95d9f4ac83fdd713dbde0ebc1616f8fea51
unknown <shem@gps-africa.com>
2026-04-14 17:25:22 +0300

    Task2

---
+++ extra.py
@@ -0,0 +1,3 @@
+my_name=input("Enter your name: ")
+print(my_name)
+print(type(my_name))
---
+++ task2.py
@@ -0,0 +1,32 @@
+# Convert a float to an integer with an inbuilt function in Python
+# temp = 56.8926 to 57
+
+temp = 56.8926
+temp = round(temp)
+print(temp)
+
+# Convert the float below to give the results as follows
+# temp = 56.8926 to 56.89 
+
+temp = 56.8926
+temp = round(temp,2)
+print(temp)
+
+# Convert the float below to give the results as follows
+# temp = 56.8926 to 56.893 
+
+temp = 56.8926
+temp = round(temp,3)
+print(temp)
+
+
+# Convert the float below to give the results as follows
+# temp=56.8926 to 8.926 
+
+temp = 56.8926
+temp1 = int(temp)
+temp = round((temp - temp1) * 10,3)
+print(temp)
+
+
+

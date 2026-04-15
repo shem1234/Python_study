58506f8b5189d39ad00f92a0f9ebc8457a0b16a6
unknown <shem@gps-africa.com>
2026-04-15 15:19:24 +0300

    Task2 udated

--- task2.py
+++ task2.py
@@ -19,14 +19,14 @@ temp = 56.8926
 temp = round(temp,3)
 print(temp)
 
-
 # Convert the float below to give the results as follows
 # temp=56.8926 to 8.926 
-
 temp = 56.8926
-temp1 = int(temp)
-temp = round((temp - temp1) * 10,3)
+temp = str(temp)
+print(type(temp))
+temp = temp[3:]
+temp = temp[0] + "." + temp[1:]
+temp = float(temp)
 print(temp)
-
 
 

cecdc22577c8ec8fcb544905901c68fe707ee890
unknown <shem@gps-africa.com>
2026-04-21 16:16:39 +0300

    Myds task

--- mydstask.py
+++ mydstask.py
@@ -15,12 +15,18 @@ my_ds[3][2]['amount']=90
 print(my_ds[3][2])
 
 # 5. Reverse 987 to 789 without using an inbuilt -method or Assigning 789 manually.
-my_ds[4]=789
+# Hint: Strings can be reversed using [::]
+my_ds[4]=str(my_ds[4])
+my_ds[4]=my_ds[4][::-1]
+my_ds[4]=int(my_ds[4])
 print(my_ds[4])
 
-#      Hint: Strings can be reversed using [::]
 # 6. Change the name “John” to “Jane” . 
-my_ds[1]='John'
-print(my_ds[1])
+print(my_ds[5])
+my_ds[5]=list(my_ds[5])
+print(my_ds[5][1])
+my_ds[5][1]="Jane"
+my_ds[5]=tuple(my_ds[5])
+print(my_ds)
 
 # You can research or discuss to find the solutions above.
